37bcc538cc0e130b9fe289c8ec8d2b5611254009
Alicja <alicjanamieta@gmail.com>
2025-11-14 19:17:08 +0100

    Add ArenaOptions interface

---
+++ frontend/src/customization/CustomizationPanel.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import ArenaSettings from './ArenaSettings';
+import BallSettings from './BallSettings';
+
+interface ArenaOptions {
+  theme: string;
+  backgroundColor: string;
+  borderStyle: string;
+}
