5904ec006c477128163d83aa56bf8a480e0cca3e
Alicja <alicjanamieta@gmail.com>
2025-11-14 23:57:27 +0100

    Add base layout and arena preview container

--- frontend/src/customization/PreviewPanel.tsx
+++ frontend/src/customization/PreviewPanel.tsx
@@ -6,3 +6,23 @@ interface PreviewPanelProps {
   ball: BallSettingsData;
   paddle: PaddleSettingsData;
 }
+
+const PreviewPanel: React.FC<PreviewPanelProps> = ({ arena, ball, paddle }) => {
+  return (
+    <div className="preview-container">
+      <div
+        className="preview-arena"
+        style={{
+          backgroundColor: arena.backgroundColor,
+          borderStyle: arena.borderStyle,
+          borderWidth: '4px',
+          borderColor: '#fff',
+          width: '300px',
+          height: '200px',
+          position: 'relative'
+        }}
+      >
+      </div>
+    </div>
+  );
+};
