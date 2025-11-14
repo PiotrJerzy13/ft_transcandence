48e05b730cbea70fe99a3adcd3883665759c0100
Alicja <alicjanamieta@gmail.com>
2025-11-14 19:20:44 +0100

    Export CustomizationProfile interface

--- frontend/src/customization/CustomizationPanel.tsx
+++ frontend/src/customization/CustomizationPanel.tsx
@@ -1,3 +1,10 @@
 import React, { useState } from 'react';
 import ArenaSettings, { ArenaOptions, BallOptions } from './ArenaSettings';
 import BallSettings from './BallSettings';
+
+export interface CustomizationProfile {
+	name: string;
+	arena: ArenaOptions;
+	ball: BallOptions;
+  }
+  
