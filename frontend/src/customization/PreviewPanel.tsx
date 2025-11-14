639a93549d779a52cb2dc1f239ce33461b6fe9ff
Alicja <alicjanamieta@gmail.com>
2025-11-15 00:07:17 +0100

    Add PreviewPanel component with arena, ball, and paddle preview

--- frontend/src/customization/PreviewPanel.tsx
+++ frontend/src/customization/PreviewPanel.tsx
@@ -22,34 +22,34 @@ const PreviewPanel: React.FC<PreviewPanelProps> = ({ arena, ball, paddle }) => {
           position: 'relative'
         }}
       >
+        <div
+          className="preview-ball"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: `${ball.size}px`,
+            height: `${ball.size}px`,
+            backgroundColor: ball.color,
+            borderRadius: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        />
+        <div
+          className="preview-paddle"
+          style={{
+            position: 'absolute',
+            bottom: '10px',
+            left: '50%',
+            width: `${paddle.width}px`,
+            height: `${paddle.height}px`,
+            backgroundColor: paddle.color,
+            transform: 'translateX(-50%)'
+          }}
+        />
       </div>
     </div>
   );
 };
-      <div
-        className="preview-ball"
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          width: `${ball.size}px`,
-          height: `${ball.size}px`,
-          backgroundColor: ball.color,
-          borderRadius: '50%',
-          transform: 'translate(-50%, -50%)'
-        }}
-      />
-      <div
-        className="preview-paddle"
-        style={{
-          position: 'absolute',
-          bottom: '10px',
-          left: '50%',
-          width: `${paddle.width}px`,
-          height: `${paddle.height}px`,
-          backgroundColor: paddle.color,
-          transform: 'translateX(-50%)'
-        }}
-      />
-	  
+
 export default PreviewPanel;
