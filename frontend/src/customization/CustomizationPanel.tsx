392019375c8c779a581bc75e7062221ddc16ac13
Alicja <alicjanamieta@gmail.com>
2025-11-14 19:20:03 +0100

    Import interfaces

--- frontend/src/customization/CustomizationPanel.tsx
+++ frontend/src/customization/CustomizationPanel.tsx
@@ -1,9 +1,3 @@
 import React, { useState } from 'react';
-import ArenaSettings from './ArenaSettings';
+import ArenaSettings, { ArenaOptions, BallOptions } from './ArenaSettings';
 import BallSettings from './BallSettings';
-
-interface ArenaOptions {
-  theme: string;
-  backgroundColor: string;
-  borderStyle: string;
-}
